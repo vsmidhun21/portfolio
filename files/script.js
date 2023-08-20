e0bd0de70b69d038131d1e3056572f7e32f5a36a
MIDHUN V S <114806736+Unluckycoder21@users.noreply.github.com>
2023-08-20 16:25:59 +0530

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
