ef4a9907984ef1228b22e188efdf9e832cb47ff7
MIDHUN V S <114806736+vsmidhun21@users.noreply.github.com>
2024-01-09 12:31:13 +0530

    Add files via upload

--- files/script.js
+++ files/script.js
@@ -7,16 +7,6 @@ var typed = new Typed(".text", {
     loop:true
 });
 
-/*
-function send(){
-    const form = document.getElementById("form");
-    let nam = document.forms["form"]["name"].value;
-    let mail = document.forms["form"]["mail"].value;
-    let sub = document.forms["form"]["sub"].value;
-    let msg = document.forms["form"]["msg"].value;
-    console.log(nam);
-}*/
-
 
 const toTop = document.querySelector(".top");
 window.addEventListener("scroll",() =>{
